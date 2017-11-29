11799c294e09cd12a8407fce64b34c1d291f3a30
Manuel <m.a.c.p90@gmail.com>
2017-11-29 14:54:49 -0500

    fixed error in session and app.module was populated

--- frontEnd-app/src/app/session.service.spec.ts
+++ frontEnd-app/src/app/session.service.spec.ts
@@ -1,46 +1,15 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import { Observable } from 'rxjs/Observable';
-// import { SessionService } from "./session.service";
+import { TestBed, inject } from '@angular/core/testing';
 
-@Injectable()
-export class SessionService {
+import { SessionService } from './session.service';
 
-  constructor(private http: Http) { }
+describe('SessionService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [SessionService]
+    });
+  });
 
-  handleError(e) {
-    return Observable.throw(e.json().message);
-  }
-
-  signup(user) {
-    return this.http.post(`/signup`, user)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-
-  login(user) {
-    return this.http.post(`/login`, user)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-
-  logout() {
-    return this.http.post(`/logout`, {})
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-
-  LoggedIn() {
-    return this.http.get(`/loggedin`)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-
-  privateData() {
-    return this.http.get(`/private`)
-      .map(res => res.json())
-      .catch(this.handleError);
-  }
-}
+  it('should be created', inject([SessionService], (service: SessionService) => {
+    expect(service).toBeTruthy();
+  }));
+});
--- frontEnd-app/src/app/session.service.ts
+++ frontEnd-app/src/app/session.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Observable';
+// import { SessionService } from "./session.service";
 
 @Injectable()
 export class SessionService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  handleError(e) {
+    return Observable.throw(e.json().message);
+  }
+
+  signup(user) {
+    return this.http.post(`/signup`, user)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  login(user) {
+    return this.http.post(`/login`, user)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  logout() {
+    return this.http.post(`/logout`, {})
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  LoggedIn() {
+    return this.http.get(`/loggedin`)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
+
+  privateData() {
+    return this.http.get(`/private`)
+      .map(res => res.json())
+      .catch(this.handleError);
+  }
 }
