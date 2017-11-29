a652641b920d94460e9be9ecf497dc6f062dd501
Manuel <m.a.c.p90@gmail.com>
2017-11-29 16:49:51 -0500

    ng2-file-upload insttals and imported in app.module

--- frontEnd-app/src/app/app.module.ts
+++ frontEnd-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {FileUploadModule} from 'ng2-file-upload';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FileUploadModule,
     BrowserModule,
      FormsModule,
      HttpModule
