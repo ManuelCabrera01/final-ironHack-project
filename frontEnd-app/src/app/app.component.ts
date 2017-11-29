a1b0d11db2acb3d088d0134ff0109fc2a06aba2d
Manuel <m.a.c.p90@gmail.com>
2017-11-29 14:57:25 -0500

    app.module populated

--- frontEnd-app/src/app/app.component.ts
+++ frontEnd-app/src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionService} from './session.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  formInfo = {
+    username: '',
+    password: ''
+  };
+    user: any;
+  error: string;
+  privateData: any = '';
+
+  constructor(private session: SessionService) { }
+
+  ngOnInit() {
+    this.session.LoggedIn()
+      .subscribe(
+        (user) => this.successCb(user)
+      );
+  }
+
+  login() {
+    this.session.login(this.formInfo)
+      .subscribe(
+        (user) => this.successCb(user),
+        (err) => this.errorCb(err)
+      );
+  }
+
+  signup() {
+    this.session.signup(this.formInfo)
+      .subscribe(
+        (user) => this.successCb(user),
+        (err) => this.errorCb(err)
+      );
+  }
+
+  logout() {
+    this.session.logout()
+      .subscribe(
+        () => this.successCb(null),
+        (err) => this.errorCb(err)
+      );
+  }
+
+  prrivateData() {
+    this.session.privateData()
+      .subscribe(
+        (data) => this.privateData = data,
+        (err) => this.error = err
+      );
+  }
+
+  errorCb(err) {
+    this.error = err;
+    this.user = null;
+  }
+
+  successCb(user) {
+    this.user = user;
+    this.error = null;
+  }
 }
