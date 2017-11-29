54da6b45e05090619c4a0c87c33dad2f7aeafe74
Manuel <m.a.c.p90@gmail.com>
2017-11-29 14:47:39 -0500

    app file populated

--- backEnd-app/app.js
+++ backEnd-app/app.js
@@ -1,15 +1,21 @@
 var express = require('express');
+var mongoose = require ('mongoose');
+
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require ('express-session');
+var passport = require('passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var authRoutes = require('./routes/auth-routes');
+require ('./config/dataBase');
+require ('./config/passport');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,9 +28,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  session({
+  secret:'sring is to avoid warning',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//routes---------------------
 app.use('/', index);
 app.use('/users', users);
+app.use('/',authRoutes );
+//------------------------
 
+app.use((req, res, next) => {
+  res.sendfile(__dirname + '/public/index.html');
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
